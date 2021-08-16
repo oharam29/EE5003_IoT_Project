e86a960e88182d288ebef9eba1872b0ad196de25
Mike <mike@debian>
2021-08-16 21:16:26 +0100

    refactor the repo to be more effiecient

--- Code/Test/filestore/startFabric.sh
+++ Code/Test/filestore/startFabric.sh
@@ -14,13 +14,13 @@ CC_SRC_LANGUAGE=${1:-"go"}
 CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
 
 if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ] ; then
-	CC_SRC_PATH="../chaincode/fabcar/go/"
+	CC_SRC_PATH="../chaincode/filestore/go/"
 elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
-	CC_SRC_PATH="../chaincode/fabcar/javascript/"
+	CC_SRC_PATH="../chaincode/filestore/javascript/"
 elif [ "$CC_SRC_LANGUAGE" = "java" ]; then
-	CC_SRC_PATH="../chaincode/fabcar/java"
+	CC_SRC_PATH="../chaincode/filestore/java"
 elif [ "$CC_SRC_LANGUAGE" = "typescript" ]; then
-	CC_SRC_PATH="../chaincode/fabcar/typescript/"
+	CC_SRC_PATH="../chaincode/filestore/typescript/"
 else
 	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
 	echo Supported chaincode languages are: go, java, javascript, and typescript
